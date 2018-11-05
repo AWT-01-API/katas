0201b5baba33f870a2de6c5fdd40db4160965d0e
Kevin Herrera <kevin.herrera@fundacion.jala.org>
2018-11-05 10:24:46 -0400

    kata 1 test, kata list 1 finished

--- src/kevinh/Kata1/Capicua.py
+++ src/kevinh/Kata1/Capicua.py
@@ -1,13 +1,9 @@
-text1 = "oruro"
-text2 = "test"
+class Capicua:
+    def __init__(self):
+        pass
 
-
-def isCapicua(word):
-    iscapicua = False
-    for x in range(0, len(word) / 2, 1):
-        iscapicua = word[x] == word[len(word) - x - 1]
-    return iscapicua
-
-
-print text1 + " is a capicua word: " + str(isCapicua(text1))
-print text2 + " is a capicua word: " + str(isCapicua(text2))
+    def checkWord(self, word):
+        iscapicua = False
+        for x in range(0, len(word) / 2, 1):
+            iscapicua = word[x] == word[len(word) - x - 1]
+        return iscapicua
