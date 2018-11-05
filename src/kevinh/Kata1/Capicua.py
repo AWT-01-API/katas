bb9e127bc5e4c639c41b593cf772b8ba050e3247
Kevin Herrera <kevin.herrera@fundacion.jala.org>
2018-11-05 10:43:15 -0400

    clean up branch

--- src/kevinh/Kata1/Capicua.py
+++
@@ -1,9 +0,0 @@
-class Capicua:
-    def __init__(self):
-        pass
-
-    def checkWord(self, word):
-        iscapicua = False
-        for x in range(0, len(word) / 2, 1):
-            iscapicua = word[x] == word[len(word) - x - 1]
-        return iscapicua
