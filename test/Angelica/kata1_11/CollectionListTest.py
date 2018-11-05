63a7655ef7dcd624bc6622dc1589cbeec7581951
Angelica Lopez <elianor@gmail.com>
2018-11-05 11:27:49 -0400

    Added first steps to kata 1 list

--- test/Angelica/kata1_11/CollectionListTest.py
+++ test/Angelica/kata1_11/CollectionListTest.py
@@ -1,20 +1,14 @@
 import unittest
-
 from src.Angelica.kata1_11.CollectionList import CollectionList
 
 
-class CollectionList(unittest.TestCase):
-    def test_init(self):
-        collection_list = CollectionList()
-        collection_list.append(3)
-        collection_list.append(4)
-        print(len(collection_list))
-        self.assertEqual(len(collection_list), 2)
+class CollectionListTest(unittest.TestCase):
 
-    def test_getElement(self):
-        collection_list = CollectionList()
-        collection_list.append(3)
-        collection_list.append(4)
-        first_element = collection_list.get_index(0)
-        print(first_element)
-        self.assertEqual(first_element, 3)
+    def setUp(self):
+        self.collection_list = CollectionList()
+        self.collection_list.append(3)
+        self.collection_list.append(2)
+
+    def test_len(self):
+        print(len(self.collection_list))
+        self.assertEqual(len(self.collection_list), 1)
