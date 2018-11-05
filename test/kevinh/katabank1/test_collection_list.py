845bfe1966a4f5ba5340526e8e51e19b19e89cca
Kevin Herrera <kevin.herrera@fundacion.jala.org>
2018-11-05 11:30:17 -0400

    added test setUp

--- test/kevinh/katabank1/test_collection_list.py
+++ test/kevinh/katabank1/test_collection_list.py
@@ -4,40 +4,34 @@ from src.kevinh.katabank1.Collections.CollectionList import CollectionList
 
 
 class TestCollectionList(unittest.TestCase):
+    def setUp(self):
+        self.collection_list = CollectionList()
 
     def test_append(self):
-        collection_list = CollectionList()
-        collection_list.append(3)
-        collection_list.append(4)
-        self.assertEqual(2, len(collection_list))
+        self.collection_list.append(3)
+        self.collection_list.append(4)
+        self.assertEqual(2, len(self.collection_list))
 
     def test_get_element(self):
-        collection_list = CollectionList()
-        collection_list.append(3)
-        collection_list.append(4)
-        collection_list.append(5)
-        collection_list.append(6)
-        self.assertEqual(3, collection_list[0])
-        self.assertEqual(4, collection_list[1])
+        self.collection_list.append(3)
+        self.collection_list.append(4)
+        self.collection_list.append(5)
+        self.collection_list.append(6)
+        self.assertEqual(3, self.collection_list[0])
+        self.assertEqual(4, self.collection_list[1])
 
     def test_set_element_at_index(self):
-        collection_list = CollectionList()
-        collection_list.append(3)
-        collection_list.append(4)
-        collection_list[1] = 100
-        collection_list[0] = 5
-        self.assertEqual(collection_list[0], 5)
-        self.assertEqual(collection_list[1], 100)
+        self.collection_list.append(3)
+        self.collection_list.append(4)
+        self.collection_list[0] = 5
+        self.collection_list[1] = 100
+        self.assertEqual(self.collection_list[0], 5)
+        self.assertEqual(self.collection_list[1], 100)
 
     def test_for_iteration(self):
-        collection_list = CollectionList()
-        collection_list.append(3)
-        collection_list.append(4)
+        self.collection_list.append(3)
+        self.collection_list.append(4)
         collection = ""
-        for value in collection_list:
+        for value in self.collection_list:
             collection += str(value)
         self.assertEqual("34", collection)
-
-
-if __name__ == '__main__':
-    unittest.main()
