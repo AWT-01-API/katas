6d12e701c09ca976d9b11b4f46f17d21ae1caead
Kevin Herrera <kevin.herrera@fundacion.jala.org>
2018-11-05 10:57:46 -0400

    sonarquebe warning fix

--- src/kevinh/Kata1/Capicua.py
+++ src/kevinh/Kata1/Capicua.py
@@ -2,7 +2,7 @@ class Capicua:
     def __init__(self):
         pass
 
-    def checkWord(self, word):
+    def checkword(self, word):
         iscapicua = False
         for x in range(0, len(word) / 2, 1):
             iscapicua = word[x] == word[len(word) - x - 1]
