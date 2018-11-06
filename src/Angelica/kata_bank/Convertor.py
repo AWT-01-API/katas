e8e24a00f2e9499b471c64168f413e3e0be2f012
Angelica Lopez <elianor@gmail.com>
2018-11-06 15:14:00 -0400

    added approaches initial to kata bank

---
+++ src/Angelica/kata_bank/Convertor.py
@@ -0,0 +1,15 @@
+import DictionaryNumber
+class Convertor:
+    def __init__(self):
+        self.file = "source_data.txt"
+        self.number = []
+        self.cond_position = 0
+        self.index_row = 3
+        self.index_bytes = 27
+
+    def reaf_file(self):
+        with open(file) as f:
+            return
+
+
+
