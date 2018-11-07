7b26f7fd00e3904d1c1e6b6c9e9c4c26b337e1f3
Angelica Lopez <elianor@gmail.com>
2018-11-06 20:45:12 -0400

    Added approach to retrieve the list of texts.

--- src/Angelica/kata_bank/Convertor.py
+++ src/Angelica/kata_bank/Convertor.py
@@ -1,15 +1,31 @@
 import DictionaryNumber
+import re
+
 class Convertor:
     def __init__(self):
-        self.file = "source_data.txt"
+        self.file = "C:\Users\AngelicaLopez\Desktop\IKata\katas\src\Angelica\kata_bank\source_data.txt"
         self.number = []
         self.cond_position = 0
         self.index_row = 3
         self.index_bytes = 27
 
     def reaf_file(self):
-        with open(file) as f:
-            return
+        with open(".\source_data.txt","r") as f:
+            content = f.readlines()
+
+        mat = []
+        aux = 0
+        for i in range(0,26):
+            if aux < 3:
+                print (content[i])
+                tmp_value = content[i]
+                mat.append(tmp_value)
+        aux += 1
+        return mat
+
+convertor = Convertor()
+convertor.reaf_file()
+
 
 
 
