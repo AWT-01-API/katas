1a4879b9ddc5e38d8696778a7ea3fbcb2d62d1b5
Kevin <kevin.sanchezpeep@gmail.com>
2018-11-11 22:55:57 -0800

    python request

--- src/kevinsanchez/python_request/RequestManager.py
+++ src/kevinsanchez/python_request/RequestManager.py
@@ -1,56 +1,57 @@
 import requests
 import json
 
-
+"""
+class to manage requests.
+"""
 class RequestManager:
 
+    """
+    constructor for header.
+    """
     def __init__(self):
         self.base_url = 'https://todo.ly/api'
-        self.header = {'X-TrackerToken': 'ea41d94388244b628a773051ff97d0a4',
+        self.header = {'X-TrackerToken': '8cabd12b477044edabc96378787fc9cd',
                        'Content-Type': 'application/json',
                        'Authorization': 'Basic a2V2aW4uc2FuY2hlenBlZXBAZ21haWwuY29tOjcyNDk1NDMxVEFOSUF0ayE='}
 
+    """
+    creates url for requests.
+    :param endpoint to construct url.
+    :return url.
+    """
     def complete_url(self, endpoint):
         return self.base_url+endpoint+'.json'
 
-    def post(self, end_point, body):
+    """
+    post requests.
+    :param endpoint for page.
+    :body to be send to url.
+    :return response.
+    """
+    def post(self, endpoint, body):
         body = json.dumps(body)
-        request = requests.post(self.complete_url(end_point),
+        request = requests.post(self.complete_url(endpoint),
                                 headers=self.header,
                                 data=body)
         return request
 
-    def get(self, end_point):
-        request = requests.get(self.complete_url(end_point),
+    """
+    get requests.
+    :param endpoint for page.
+    :return response.
+    """
+    def get(self, endpoint):
+        request = requests.get(self.complete_url(endpoint),
                                headers=self.header)
         return request
 
-    def delete(self, end_point):
-        request = requests.delete(self.complete_url(end_point),
+    """
+    delete request.
+    :param endpoint for page.
+    :return response.
+    """
+    def delete(self, endpoint):
+        request = requests.delete(self.complete_url(endpoint),
                                   headers=self.header)
         return request
-
-    def get_a_project_(self, id_project):
-        request_type = requests.get('https://todo.ly/api/projects/' + str(id_project) + '.json', headers=self.header)
-        return request_type
-
-    def get_all_projects(self):
-        request_type = requests.get('https://todo.ly/api/projects.json', headers=self.header)
-        return request_type
-
-    def create_project(self, data_project):
-        request_type = requests.post('https://todo.ly/api/projects.json',
-                                     headers=self.header,
-                                     data=json.dumps(data_project))
-        return request_type
-
-    def update_project(self, data_project):
-        request_type = requests.post('https://todo.ly/api/projects/' + data_project['Id'] + '.json',
-                                     headers=self.header,
-                                     data=json.dumps(data_project))
-        return request_type
-
-    def delete_project(self, id_project):
-        request_type = requests.delete('https://todo.ly/api/projects/' + str(id_project) + '.json', headers=self.header)
-        return request_type
-
--- test/kevinsanchez/python_request/TestRequestManager.py
+++ test/kevinsanchez/python_request/TestRequestManager.py
@@ -1,21 +1,70 @@
 import unittest
 from src.kevinsanchez.python_request.RequestManager import RequestManager
 
+"""
+class for test.
+"""
 class TestRequestManager(unittest.TestCase):
 
+    """
+    set up for test.
+    """
     def setUp(self):
         self.request_manager = RequestManager()
 
+    """
+    test post request for project and item.
+    """
     def test_post(self):
-        body = {'Content': 'testApi'}
-        response = self.request_manager.post('/project', body)
+        # post for project
+        body = {'Content': 'Test Project'}
+        response = self.request_manager.post('/projects', body)
+        self.assertEqual(200, response.status_code)
+        # post for item
+        body = {'Content': 'Test Item',
+                'ProjectId': response.json()['Id']}
+        response = self.request_manager.post('/items', body)
         self.assertEqual(200, response.status_code)
 
+    """
+    test get request for project and item.
+    """
     def test_get(self):
-        project_id = '3752200'
-        response = self.request_manager.get('/project/'+project_id)
+        # get for project
+        # create project to get it with the id
+        body = {'Content': 'Test Project to get'}
+        project_id = self.request_manager.post('/projects', body).json()['Id']
+
+        response = self.request_manager.get('/projects/' + str(project_id))
         self.assertEqual(200, response.status_code)
 
+        # get for item
+        # create a item to get it with the id
+        body_item = {'Content': 'Test Item',
+                     'ProjectId': response.json()['Id']}
+        item_id = self.request_manager.post('/items', body_item).json()['Id']
+
+        response_item = self.request_manager.get('/items/' + str(item_id))
+        self.assertEqual(200, response_item.status_code)
+
+    """
+    test delete request for project and item.
+    """
     def test_delete(self):
-        response = self.request_manager.delete('/project/' + project_id)
+        # create project to delete
+        body = {'Content': 'Test Project to delete'}
+        project_id = self.request_manager.post('/projects', body).json()['Id']
+
+        # create item to delete
+        body_item = {'Content': 'Test Item to delete',
+                     'ProjectId': project_id}
+        item_id = self.request_manager.post('/items', body_item).json()['Id']
+
+        # delete item
+        response_item = self.request_manager.delete('/items/' + str(item_id))
+        self.assertEqual(200, response_item.status_code)
+
+        # delete project
+        response = self.request_manager.delete('/projects/' + str(project_id))
         self.assertEqual(200, response.status_code)
+
