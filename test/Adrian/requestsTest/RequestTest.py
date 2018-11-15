782260221e1e22751b80a0babec79c4964838da5
Adrian Rojas <adrian.rojas@fundacion-jala.org>
2018-11-15 17:11:13 -0400

    clean tests

--- test/Adrian/requestsTest/RequestTest.py
+++ test/Adrian/requestsTest/RequestTest.py
@@ -41,8 +41,5 @@ class RequestTet(TestCase):
         self.assertEqual("<Response [500]>", str(r))
 
     def test_create_project_create_task_inside_assert_delete_both(self):
-        # TODO test
-        # Create new project add task, check if task exists, check if its undone,
-        # set as finished, delete project"
         r = self.requests.create_project("New Project" + str(datetime.datetime.now()), "3")
         self.assertEqual("<Response [200]>", str(r))
